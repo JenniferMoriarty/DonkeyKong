a0b5167b51ba121adf3c0545c1d5330864f723aa
Jen Moriarty <JMoriarty78@gmail.com>
2019-12-27 12:35:57 -0700

    Create gameObject.cpp

---
+++ gameObject.cpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "GameObject.h"
+
+
+//virtual functions
+void gameObject::move(vector<gameObject*>stuff) {}
+void gameObject::draw() {}
+
+//regular functions
+int gameObject::getX() { return xPos; }
+int gameObject::getY() { return yPos; }
+int gameObject::getID() { return ID; }
+
